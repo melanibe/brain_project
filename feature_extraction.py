f17572eb28bcc2b683c91cceabe25512973e6261
melanibe <melanibe@student.ethz.ch>
2018-07-19 16:00:03 +0200

    added threshold parameters

--- feature_extraction.py
+++ feature_extraction.py
@@ -8,13 +8,15 @@ from scipy.stats import ttest_ind
 
 cwd = os.getcwd()
 
-def extract_opt_2class(name='std', y = 'y', graph=False):
+def extract_opt_2class(name='std', y = 'y', graph=False, p_threshold=0.05):
     if name=='X':
         X,Y = prepare_X()
     else:
         Y = np.load(cwd+'/{}.npy'.format(y))
-        X = np.reshape(np.load(cwd+'/{}.npy'.format(name)),(-1,4095,5))
-    m, _, nfreq = np.shape(X)
+        X = np.load(cwd+'/{}.npy'.format(name))
+        m, _ = np.shape(X)
+        X = np.reshape(X,(m, 4095, -1))
+    _, _, nfreq = np.shape(X)
     Y = [1 if ((y==1) or (y==2)) else 0 for y in Y]
     idx0 = [False if ((y==1) or (y==2)) else True for y in Y]
     idx1 = [True if ((y==1) or (y==2)) else False for y in Y]
@@ -28,7 +30,7 @@ def extract_opt_2class(name='std', y = 'y', graph=False):
             print(s)
         for f in range(nfreq):
             _, p = ttest_ind(X0[:,s,f], X1[:,s,f],equal_var=False)
-            if p < 0.05:
+            if p < p_threshold:
                 l.append(np.ravel_multi_index((s,f), (4095,nfreq)))
     n= len(l)
     print(len(l))
@@ -37,7 +39,7 @@ def extract_opt_2class(name='std', y = 'y', graph=False):
     if graph:
         X_new = np.zeros((m, 4095, nfreq))
         X_new[:,l[0], l[1]] = X[:,l[0], l[1]]
-        return(X_new)
+        return(X_new, Y)
     else:
         np.save(cwd+'/{}_sel'.format(name), np.reshape(X[:,l[0], l[1]],(-1,n)))
 
