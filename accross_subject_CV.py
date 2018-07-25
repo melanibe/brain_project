841a7774303b565ab0ba15454db70e669d1ac2a0
melanibe <melanibe@student.ethz.ch>
2018-07-25 19:09:11 +0200

    to fit new accross subjet

--- accross_subject_CV.py
+++ accross_subject_CV.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
-from build_features import index_subj
+
 from sklearn.metrics import roc_auc_score, accuracy_score
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import VarianceThreshold, SelectPercentile
@@ -11,6 +11,8 @@ from sklearn.ensemble import RandomForestClassifier
 import logging
 import time 
 import argparse
+
+from CV_utils import AcrossSubjectCV
 
 cwd = os.getcwd()
 subject_list = ['S01', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', 'S10', 'S11', 'S12']
@@ -46,62 +48,25 @@ file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
-###### ACROSS CV #####
-def AcrossSubjectCV(estimator, X, Y):
-    """ Implements custom CV to calculate the across subject accuracy.
-    10-fold CV since we have 10 subjects.
-    """
-    p = pd.read_pickle(cwd+'/subj_list')
-    roc_auc=[]
-    accuracyCV = []
-    for s in subject_list:
-        idx, subj_idx = p.loc[p['subj']==s, 'idx'].values[0], p.loc[p['subj']==s, 'subj_idx'].values[0]
-        print("Preparing fold only {}".format(s))
-        X_test, Y_test = [X[i] for i in subj_idx], [Y[i] for i in subj_idx]
-        print("Preparing fold except {}".format(s))
-        X_train, Y_train = [X[i] for i in idx], [Y[i] for i in idx]
-        n = len(X_train)
-        print(n)
-        print(len(X_test))
-        assert(len(X_train)+len(X_test)==10081)
-        # perform upsampling only on training fold
-        neg_ix = np.where([Y_train[i]==0 for i in range(n)])[0]
-        pos_ix = np.where([Y_train[i]==1 for i in range(n)])[0]
-        assert(len(pos_ix)+len(neg_ix)==n)
-        aug_pos_ix = np.random.choice(pos_ix, size=len(neg_ix), replace=True)
-        X_train = np.reshape(np.append([X_train[i] for i in neg_ix], [X_train[i] for i in aug_pos_ix]),(len(neg_ix)+len(aug_pos_ix),-1))
-        Y_train = np.append([0 for i in neg_ix], [1 for i in aug_pos_ix])
-        print(np.shape(X_train))
-        print("Fit the estimator")
-        estimator.fit(X_train, Y_train)
-        print("Calculating the metrics")
-        roc_auc.append(roc_auc_score(Y_test, estimator.predict_proba(X_test)[:,1]))
-        accuracyCV.append(accuracy_score(Y_test, estimator.predict(X_test)))
-        print(roc_auc[-1], accuracyCV[-1])
-    metrics = pd.DataFrame()
-    metrics['auc'] = roc_auc
-    metrics['accuracy'] = accuracyCV
-    metrics.index = subject_list
-    results = pd.DataFrame()
-    results['mean'] = [np.mean(roc_auc), np.mean(accuracyCV)]
-    results['std'] = [np.std(roc_auc), np.std(accuracyCV)]
-    results['min'] = [np.min(roc_auc), np.min(accuracyCV)]
-    results['max'] = [np.max(roc_auc), np.max(accuracyCV)]
-    results.index=['roc_auc', 'accuracy']
-    return(results, metrics)
+
 
 ######### best with graph
 estimator_graph = Pipeline([('var', VarianceThreshold(threshold=0)), \
                             ('std', StandardScaler()), \
                             ('PerBest', SelectPercentile(percentile=50)), \
-                            ('rf', RandomForestClassifier(n_estimators=10000, min_samples_split=30, n_jobs=njobs))])        
+                            ('rf', RandomForestClassifier(n_estimators=10, min_samples_split=30, n_jobs=njobs))])        
 matrix_graph = np.load(cwd+'/graph_features/std_tresh0.05.npy')
 Y = np.load(cwd+'/y.npy')
 Y_main = [1 if ((y==1) or (y==2)) else 0 for y in Y]
-tmp = AcrossSubjectCV(estimator_graph, matrix_graph, Y_main)
+list = ['S04', 'S10', 'S12', 'S05']
+tmp = AcrossSubjectCV(estimator_graph, matrix_graph, Y_main, list )
 logger.info("Global results for best graph feature estimator from accross subject CV are: \n"+tmp[0].to_string())
 print(tmp[1])
+for i in range(len(list)):
+    logger.info("Confusion matrix for subject {} as test is: \n".format(list[i])+pd.DataFrame(tmp[2][i]).to_string())
+logger.info("Mean of confusion matrices from accross subject CV are: \n {}".format(pd.DataFrame(np.mean(tmp[2], 0)).to_string()))
 logger.info("Results per subject for best graph feature estimator from accross subject CV are: \n"+tmp[1].to_string())
+
 
 ########### best with orig
 matrix_full = np.load(cwd+'/X_sel.npy')
